35f9db7fd694d113f073e45916f930f1bf997d24
dansodanso28 <dansodanso25@gmail.com>
2025-04-19 17:25:44 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const menu = document.getElementById("navmenu");
+  menu.classList.toggle("show");
+});
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+const menu = document.getElementById("navmenu");
+menu.classList.toggle("show");
+});
